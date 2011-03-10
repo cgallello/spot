83fc57b791a5ff6e3aac1ae36d23b085145a2c1f
unknown <cgallello@.olin.edu>
2011-03-09 19:15:01 -0500

    created rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spot::Application.config.secret_token = 'd518ce0244d939f7734f54b45a6a701b06d35e02258b648e8505f5ac20baec0f97f4c801bb0a90765048a9286114a434063ec9f9d17cdfe9c6c7286e6c561a5b'
